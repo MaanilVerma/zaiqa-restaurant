35b6334f70c86f93047ff20fec3829f9a25ae363
Maanil Verma <maanilv8@gmail.com>
2022-11-04 11:49:27 +0530

    Update Static Items Prices

--- src/utils/showcaseStatic.tsx
+++ src/utils/showcaseStatic.tsx
@@ -1,33 +1,32 @@
-import {Chicken, Fish, Icecream, Strawberry} from "../components/Assets";
+import { Chicken, Fish, Icecream, Strawberry } from "../components/Assets";
 
 export const data = [
-
-    {
-        id: 2,
-        title: 'Strawberries',
-        desc: "Fresh Strawberries",
-        price: '7.25',
-        imgSrc: Strawberry
-    },
-    {   
-        id: 3,
-        title: 'Chicken',
-        desc: "Mixed Kebab",
-        price: '15.65',
-        imgSrc: Chicken
-    },
-    {
-        id: 4,
-        title: 'Tilapia',
-        desc: "Roasted Tilapia",
-        price: '10.25',
-        imgSrc: Fish
-    },{
-        id: 1,
-        title: 'Icream',
-        desc: "Chocolate & Vanila",
-        price: '5.25',
-        imgSrc: Icecream
-    },
-
-]
+  {
+    id: 2,
+    title: "Strawberries",
+    desc: "Fresh Strawberries",
+    price: "120",
+    imgSrc: Strawberry,
+  },
+  {
+    id: 3,
+    title: "Chicken",
+    desc: "Mixed Kebab",
+    price: "299",
+    imgSrc: Chicken,
+  },
+  {
+    id: 4,
+    title: "Fish",
+    desc: "Roasted Fish",
+    price: "599",
+    imgSrc: Fish,
+  },
+  {
+    id: 1,
+    title: "Icream",
+    desc: "Chocolate & Vanila",
+    price: "120",
+    imgSrc: Icecream,
+  },
+];
